c3d2e3caf36e62a36638c4e306d743fd8b418669
Bartosz <bartosz.lesniatynski@gmail.com>
2018-01-21 00:41:25 +0100

    Change command

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 setup (
-	name="endpoints",
+	name="getend",
 	version='0.2',
 	py_modules=['main'],
 	install_requires=[
 		'Click',
+		'colorama'
 	],
 	entry_points='''
 		[console_scripts]
