c866f8826a1926decd2c4f5df5048ca617671ace
Bartosz <bartosz.lesniatynski@gmail.com>
2018-01-21 02:01:23 +0100

    Misc

--- main.py
+++ main.py
@@ -57,12 +57,10 @@ def printGrep(file, grepString):
 
 #setup
 @click.command()
-@click.option('--name', default=None, help="Print controller")
 @click.option('--l', is_flag=True, help="List all controllers")
+@click.option('--name', default=None, help="Print controller")
 @click.option('--grep', default=None, help="Print all lines containing string")
 def cli(name, l, grep):
-    click.echo("Rollin...")
-    sleep(0.5)
     path = "controller"
     controllerFound = False
     grepFound = False
@@ -78,13 +76,14 @@ def cli(name, l, grep):
             elif grep is not None and name is not None:
                 if(name in filename):
                     controllerFound = True
+                    click.echo("Found controller "+filename)
                     grepFound = printGrep(file, grep)
             elif grep is not None:
                 if grepFound is False:
                     grepFound = printGrep(file, grep)
                 else:
                     printGrep(file,grep)
-                controllerFound=True
+                    controllerFound=True
             else:
                 if name is not None and name in filename:
                     controllerFound = True
