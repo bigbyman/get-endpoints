726d08088187f2afe7a86cdb3ab5d6b5b4e70435
Bartosz <bartosz.lesniatynski@gmail.com>
2018-01-21 01:55:23 +0100

    Fix

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ def printController (file):
     
     for line in file:
 
-        sleep(0.1)
         if("@RequestMapping" in line):
             click.echo(Fore.YELLOW + Style.BRIGHT + line.strip(' \n') + Style.RESET_ALL)
             click.echo('{')
+            sleep(0.5)
 
         elif("@GetMapping" in line):
             click.echo('\n'+Fore.YELLOW + Style.BRIGHT +'\t'+line.strip(' \n') + Style.RESET_ALL)
             nextLine = next(file)
             click.echo('\t'+nextLine.replace('public','').replace('{','').strip() + Style.RESET_ALL)
+            sleep(0.1)
             
         elif("@PostMapping" in line):
             click.echo('\n'+Fore.YELLOW + Style.BRIGHT +'\t'+line.strip(' \n') + Style.RESET_ALL)
             nextLine = next(file)
             click.echo('\t'+nextLine.replace('public','').replace('{','').strip() + Style.RESET_ALL)
+            sleep(0.1)
             
         elif("@RequestParam" in line):
             click.echo('\t'+Fore.YELLOW + Style.BRIGHT +'\t'+line.strip(' \n') + Style.RESET_ALL)
+            sleep(0.1)
 
 
     click.echo('}')
@@ -48,7 +51,7 @@ def printGrep(file, grepString):
             if first is True:
                 click.echo(Style.BRIGHT)
                 first=False
-            click.echo(Fore.GREEN + filename.replace('.java','') + '\t ' + Style.BRIGHT + line.replace('public','').strip(' \n')+Style.RESET_ALL)
+            click.echo(Fore.GREEN + file.name.replace('.java','') + '\t ' + Style.BRIGHT + line.replace('public','').strip(' \n')+Style.RESET_ALL)
             grepFound = True
     return grepFound
 
@@ -66,7 +69,7 @@ def cli(name, l, grep):
     fileCounter=1
     if name:
         click.echo("Searching for "+name+" controller")
-        sleep(0.5)
+        sleep(1)
     for filename in os.listdir(path):
         with open(path+'/'+filename) as file: 
             if l:
@@ -88,6 +91,6 @@ def cli(name, l, grep):
                     click.echo(Style.BRIGHT + Fore.GREEN + filename)
                     printController(file)
     if controllerFound is False and name is not None:
-        click.echo(Fore.RED + Style.BRIGHT+"Controller "+name+" not found")
-    if grepFound is False and grep is True:
-        click.echo(Fore.RED + Style.BRIGHT+"Grep "+grep+" not found")
+        click.echo(Fore.RED + Style.BRIGHT+"Controller "+name+" not found"+Style.RESET_ALL)
+    if grepFound is False and grep is not None:
+        click.echo(Fore.RED + Style.BRIGHT+"Grep \'"+Fore.WHITE+grep+Fore.RED+"\' not found"+Style.RESET_ALL)
